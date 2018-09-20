09a8dca88f178380331af6752ae8ea0db5d47c4b
mingming <hust2ming@163.com>
2018-09-20 11:50:10 +0800

    修改bug

--- src/confused_package/orz/TowToOne.java
+++ src/confused_package/orz/TowToOne.java
@@ -1,19 +1,23 @@
 package confused_package.orz;
 
 public class TowToOne {
-	 public static String longest (String s1, String s2) {
+	 public static String longest (String s1, String s2) throws StringIndexOutOfBoundsException{
 	        String str =s1+s2;
-	        for(int i=0;i<str.length();i++) {
-	        	for(int j=1;j<str.length();j++) {
+	        StringBuffer sb =new StringBuffer(str);
+	        for(int i=0;i<sb.length();i++) {
+	        	for(int j=i+1;j<sb.length();j++) {
 //	        		System.out.println(str.subSequence(i,i+1));
 //	        		System.out.println(str.subSequence(j,j+1));
-	        	if(str.substring(i, i+1)==str.substring(j, j+1)) {
-	        		str.replace(str.substring(j,j+1),"");
+	        	if(sb.charAt(i)==sb.charAt(j)) {
+	        		 sb.deleteCharAt(j);
+	        		 j--;
+//	        		 System.out.println(sb);
 	        	}
 	          }
 	        }
-	        return str;
-	    }
+	      
+			return sb.toString();
+}
 	 public static void main(String[]args) {
 		 System.out.println(longest("abday","dayyou"));
 	 }
